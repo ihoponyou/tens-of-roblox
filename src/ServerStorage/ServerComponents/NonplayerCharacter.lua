01dfee1d53e893565c0e7fae854e211d1fe5f585
noahmagcalas <noah.magcalas@gmail.com>
2023-12-31 14:38:25 -0600

    working on npc targeting

--- src/ServerStorage/ServerComponents/NonplayerCharacter.lua
+++ src/ServerStorage/ServerComponents/NonplayerCharacter.lua
@@ -1,5 +1,6 @@
 
 local CollectionService = game:GetService("CollectionService")
+local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerStorage = game:GetService("ServerStorage")
 
@@ -43,8 +44,14 @@ function NonplayerCharacter:Construct()
 	habitat.Parent = self.Instance
 	habitat.Value = self.Habitat
 
+	self._target = nil
 	self._destination = Vector3.new()
-	self._travelDelay = RAND:NextNumber(1, 5)
+	self._heartbeatDelay = RAND:NextNumber(1, 5)
+
+	self._searchParams = OverlapParams.new()
+	self._searchParams.CollisionGroup = "Character"
+	self._searchParams.FilterDescendantsInstances = { self.Instance }
+	self._searchParams.FilterType = Enum.RaycastFilterType.Exclude
 end
 
 function NonplayerCharacter:Start()
@@ -53,15 +60,38 @@ function NonplayerCharacter:Start()
 	self._trove:Add(Timer.Simple(self._travelDelay, function()
 		if self.Instance:GetAttribute("Knocked") then return end
 
-		self._travelDelay = RAND:NextNumber(1, 5)
+		if self._target ~= nil then
+			self._travelDelay = 0.5
+			self._destination = self._target.PrimaryPart.Position
+		else
+			self:SearchForTarget()
+			self._travelDelay = RAND:NextNumber(1, 5)
+			self:_calculateDestination()
+		end
 
-        self:_calculateDestination()
 		self.Humanoid:MoveTo(self._destination)
     end, true))
 end
 
 function NonplayerCharacter:Stop()
 	self._trove:Destroy()
+end
+
+function NonplayerCharacter:HeartbeatUpdate(deltaTime: number)
+	self._heartbeatTime += deltaTime
+end
+
+function NonplayerCharacter:SearchForTarget()
+	for _,player in Players:GetPlayers() do
+		local character = player.Character
+		if not character then continue end
+
+		local distance = (character.PrimaryPart.Position - self.Instance.PrimaryPart.Position).Magnitude
+		if distance < 10 then
+			print("target found")
+			self._target = character
+		end
+	end
 end
 
 function NonplayerCharacter:_calculateDestination()
