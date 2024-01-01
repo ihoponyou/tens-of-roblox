dc11c9d78038932b35c07be1099d294d921cdf14
noahmagcalas <noah.magcalas@gmail.com>
2023-12-31 20:41:02 -0600

    npc state

--- src/ServerStorage/ServerComponents/NonplayerCharacter.lua
+++ src/ServerStorage/ServerComponents/NonplayerCharacter.lua
@@ -29,24 +29,27 @@ function NonplayerCharacter:Construct()
 	self._trove = Trove.new()
 
 	self.Habitat = self.Instance.Parent
+	local habitat = Instance.new("ObjectValue")
+	habitat.Name = "Habitat"
+	habitat.Parent = self.Instance
+	habitat.Value = self.Habitat
 
 	self.Humanoid = Instance.new("Humanoid")
 	self.Humanoid.Parent = self.Instance
-	self.Humanoid.WalkSpeed = 8
+	self.Humanoid.WalkSpeed = RAND:NextInteger(8, 24)
 	self.Humanoid.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.Subject
 	self.Humanoid.NameDisplayDistance = 10
 	self.Humanoid.HealthDisplayDistance = 10
 	self.Humanoid:ApplyDescriptionReset(CHARACTER_FOLDER.NoobDescription)
 	self._trove:Add(self.Humanoid)
 
-	local habitat = Instance.new("ObjectValue")
-	habitat.Name = "Habitat"
-	habitat.Parent = self.Instance
-	habitat.Value = self.Habitat
-
+	self.CurrentState = function() print("override this") end
 	self._target = nil
 	self._destination = Vector3.new()
+	-- time between npc heartbeats
 	self._heartbeatDelay = RAND:NextNumber(1, 5)
+	-- time that has passed since last npc heartbeat
+	self._heartbeatTime = 0
 
 	self._searchParams = OverlapParams.new()
 	self._searchParams.CollisionGroup = "Character"
@@ -57,20 +60,7 @@ end
 function NonplayerCharacter:Start()
 	self.Knockable = self:GetComponent(Knockable)
 
-	self._trove:Add(Timer.Simple(self._travelDelay, function()
-		if self.Instance:GetAttribute("Knocked") then return end
-
-		if self._target ~= nil then
-			self._travelDelay = 0.5
-			self._destination = self._target.PrimaryPart.Position
-		else
-			self:SearchForTarget()
-			self._travelDelay = RAND:NextNumber(1, 5)
-			self:_calculateDestination()
-		end
-
-		self.Humanoid:MoveTo(self._destination)
-    end, true))
+	self.CurrentState = self.Wandering
 end
 
 function NonplayerCharacter:Stop()
@@ -79,6 +69,10 @@ end
 
 function NonplayerCharacter:HeartbeatUpdate(deltaTime: number)
 	self._heartbeatTime += deltaTime
+	if self._heartbeatTime < self._heartbeatDelay then return end
+	self._heartbeatTime = 0
+
+	self:CurrentState()
 end
 
 function NonplayerCharacter:SearchForTarget()
@@ -87,11 +81,37 @@ function NonplayerCharacter:SearchForTarget()
 		if not character then continue end
 
 		local distance = (character.PrimaryPart.Position - self.Instance.PrimaryPart.Position).Magnitude
-		if distance < 10 then
-			print("target found")
+		if distance <= 50 then
+			print(self.Instance.Name..": target found")
 			self._target = character
 		end
 	end
+end
+
+function NonplayerCharacter:Wandering()
+	self:SearchForTarget()
+
+	if self._target == nil then
+		self._heartbeatDelay = RAND:NextNumber(1, 3)
+		self:_calculateDestination()
+		self.Humanoid:MoveTo(self._destination)
+	else
+		self.CurrentState = self.Following
+	end
+end
+
+function NonplayerCharacter:Jumping()
+	
+end
+
+function NonplayerCharacter:Following()
+	self._heartbeatDelay = 0.1
+
+	local targetRoot = self._target.PrimaryPart
+	local targetVelocity = targetRoot.AssemblyLinearVelocity
+	local projectedPosition = targetRoot.CFrame.Position + targetVelocity
+
+	self.Humanoid:MoveTo(projectedPosition)
 end
 
 function NonplayerCharacter:_calculateDestination()
